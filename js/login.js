1d30f752b20e91c757702c4f163299ad238a911d
Shamim Reza <wpshamim@gmail.com>
2022-03-31 22:51:45 +0600

    add demo user & password

--- js/login.js
+++ js/login.js
@@ -2,10 +2,7 @@ const loginBtn = document.getElementById("login-btn");
 loginBtn.addEventListener("click", () => {
     const passwordField = loginBtn.previousElementSibling;
     const emailField = passwordField.previousElementSibling;
-    if (
-        emailField.value == "wpshamim@gmail.com" &&
-        passwordField.value == "12345"
-    ) {
+    if (emailField.value == "demo@bank.com" && passwordField.value == "12345") {
         window.location.href = "/banking.html";
     } else {
         passwordField.style.border = "1px solid red";
